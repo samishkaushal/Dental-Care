e4398f9aea12eaf232107cf137fcb11ec400a1cc
Samish <samishkaushal26gmail.com>
2026-06-04 13:57:02 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('a[href="#book-audit"]').forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log("Free Audit Button Clicked");
+    });
+});
+document.querySelectorAll('a[href="#book-audit"]').forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log("Free Audit Button Clicked");
+    });
+});
+
+function handleSubmit() {
+
+    const btn = document.querySelector(".form-submit");
+
+    btn.innerText = "✅ Request Submitted";
+    btn.disabled = true;
+
+    alert("Thank You! We will contact you soon.");
+}
+function handleSubmit() {
+    alert("Thank You! Your Audit Request Has Been Submitted.");
+
+    window.open(
+        "https://wa.me/918960473346",
+
+        "_blank"
+    );
+}
