021ff36d25c2d05e17cf819bdea38bdb4dcac57f
Samish <samishkaushal26gmail.com>
2026-06-11 18:10:49 -0300

    updated hero section UI and responsive fixes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.querySelectorAll('a[href="#book-audit"]').forEach(btn => {
+document.querySelectorAll('a[href="#whatsapp-us"]').forEach(btn => {
     btn.addEventListener('click', () => {
-        console.log("Free Audit Button Clicked");
+        console.log("whatsapp");
     });
 });
 document.querySelectorAll('a[href="#book-audit"]').forEach(btn => {
@@ -27,3 +27,20 @@ function handleSubmit() {
         "_blank"
     );
 }
+// function handleSubmit() {
+//     const btn = document.querySelector(".form-submit");
+
+//     btn.innerText = " Request Submitted";
+//     btn.disabled = true;
+
+//     alert("Thank You! We will contact you soon.");
+// }
+
+// function handleSubmit() {
+//     alert("Thank You! Your Audit Request Has Been Submitted.");
+
+//     window.open(
+//         "https://wa.me/918960473346",
+//         "_blank"
+//     );
+// }
